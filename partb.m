69cdc6c987b59863b52cad4c15c9b77c5247a562
Adam Sherwood <ball6543@ox.ac.uk>
2025-11-02 19:40:12 +0000

    Cleanup and commenting

--- partb.m
+++ partb.m
@@ -23,7 +23,7 @@ rng(1) % Set seed for reproducibility
 sigma = 0.01; % Noise Standard Deviation
 
 if useNoise
-    y = y_true_signal + sigma * randn(size(y_true_signal)); % Add Noise
+    y = y_true_signal + sigma * randn(size(y_true_signal)); % Adds Noise if required
 else
     y = y_true_signal;
 end
@@ -31,22 +31,29 @@ end
 
 %% Vandermonde Polynomial Fitting
 
-Yfit = cell(10,1);
-Coefficients = cell(10,1);
+maxDeg = 10 % Highest degree polynomial
+
+% Cells to store results of Vandermonde Fitting
+Yfit = cell(maxDeg,1); % Polynomial curves
+Coefficients = cell(maxDeg,1); % Coefficients
+condV = cell(maxDeg,1); % Condition number of V'V
 
 N = length(t);
 
-for m = 1:10
+for m = 1:maxDeg
+
     V = zeros(N,m+1);
     
+    % Build Vandermonde matrix [1, x, ... x^m]
     for i = 0:m
-        V(:, i+1) = t.^i;
+        V(:, i+1) = t.^i; 
     end
 
-    c = V \ y';
+    c = V \ y'; % Least squares fit
 
-    Coefficients{m} = c;
-    Yfit{m} = V * c;
+    Coefficients{m} = c; % Store Coefficients
+    Yfit{m} = V * c; % Store Polynomial curve    
+    condV{m} = cond(V' * V); % Store condition number V'V
 end
 
 
